a472d8983a4bedd7140652eceac5d42a637d3b79
DmytroSablin <dmytrosablin@gmail.com>
2023-05-01 20:08:38 +0300

    last bagfix commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,27 @@
+import bg1 from "./../assets/bg1.jpg"
+import bg2 from "./../assets/bg2.jpg"
+import bg3 from "./../assets/bg3.jpg"
+
+
 const Footer = (props) => {
     const FullScreen = () => {
         document.body.requestFullscreen()
     }
     return (
         <div className="grid grid-cols-footer">
-            <div className="bg-bg1 bg-cover">
-                <button onClick={FullScreen} className="bg-emerald-300 w-10 h-7 mt-4 ml-16"></button>
+            <div className="bg-cover" style={{
+                backgroundImage: `url(${bg1})`
+            }}>
+                <button onClick={FullScreen} className="w-10 h-7 mt-4 ml-16"></button>
             </div>
             <div onClick={props.videoon} className=""></div>
-            <div onClick={props.videoon} className="bg-bg2 bg-cover"></div>
+            <div onClick={props.videoon} className={`bg-cover`} style={{
+                backgroundImage: `url(${bg2})`
+            }}></div>
             <div onClick={props.videoon} className=""></div>
-            <div onClick={props.videoon} className="bg-bg3 bg-cover"></div>
+            <div onClick={props.videoon} className="bg-cover" style={{
+                backgroundImage: `url(${bg3})`
+            }}></div>
         </div>
     )
 }
